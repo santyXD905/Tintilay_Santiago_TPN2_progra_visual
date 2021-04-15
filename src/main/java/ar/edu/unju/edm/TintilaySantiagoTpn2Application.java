3d3b171f7e819f5660baf78f124faf8d2306259f
santy <santy@DESKTOP-F3QSP5D>
2021-04-15 16:48:40 -0300

    primer version del TP 2 de programacion visual Tintilay Santiago

---
+++ src/main/java/ar/edu/unju/edm/TintilaySantiagoTpn2Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TintilaySantiagoTpn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TintilaySantiagoTpn2Application.class, args);
+	}
+
+}
